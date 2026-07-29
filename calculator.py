de734c080adba6e23a3520e73b1fd72accb5acb7
Aditya Kumar <adityathakur0838@gmail.com>
2026-07-29 22:00:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+print("1-Addition")
+print("2-Subtraction")
+print("3-Multiplication")
+print("4-Division")
+option = int(input("Choose an operation: "))
+if(option in[1,2,3,4]):
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    if (option == 1):
+        result = num1 + num2
+    elif (option == 2):
+        result = num1 - num2
+    elif (option == 3):
+        result = num1 * num2
+    elif (option == 4):
+        result = num1 / num2
+else:
+    print("Invalid option")
+print("The result of the operation is {} ".format (result))    
---
+++ hello.py
@@ -0,0 +1,36 @@
+a = (8+2j)
+b = True
+c = "hey"
+a1 = 9
+print("the type of a is", type(a))
+print(type(b))
+print(type(c))
+print(a+a1)
+List1= [8, 2.3, [-4,5], ["apple", "banana"]]
+print(List1)
+#operators
+
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(5//3)
+print(5%3)
+print(5**3)
+
+n=15
+m=7
+ans1 = n+m
+print("addition of " ,n, "and" ,m, "is", ans1)
+ans2 = n-m
+print("subtraction of " ,n, "and" ,m, "is", ans2)
+ans3 = n*m
+print("multiplication of " ,n, "and" ,m, "is", ans3)
+ans4 = n/m
+print("division of " ,n, "and" ,m, "is", ans4)
+ans5 = n//m
+print("floor division of " ,n, "and" ,m, "is", ans5)
+ans6 = n%m
+print("modulus of " ,n, "and" ,m, "is", ans6)
+ans7 = n**m
+print("exponentiation of " ,n, "and" ,m, "is", ans7)
